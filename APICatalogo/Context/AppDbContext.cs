6abb23af46ce798b47ba7a66481b7d074a1a015d
hajikazuo <hajikazuo@outlook.com>
2024-02-15 14:26:04 -0300

    Add project files.

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
