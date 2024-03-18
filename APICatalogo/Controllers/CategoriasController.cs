9c5e0f9993c2cb3b71cb81be1844fe0eb39ccc2c
hajikazuo <hajikazuo@outlook.com>
2024-03-18 17:55:47 -0300

    middlewares, filtros e logging

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
 ﻿using APICatalogo.Context;
+using APICatalogo.Filters;
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,23 +12,41 @@ namespace APICatalogo.Controllers
     public class CategoriasController : ControllerBase
     {
         private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger _logger;
 
-        public CategoriasController(AppDbContext context)
+        public CategoriasController(AppDbContext context, IConfiguration configuration, ILogger<CategoriasController> logger)
         {
             _context = context;
+            _configuration = configuration;
+            _logger = logger;
         }
+
+        //[HttpGet("LerArquivoConfiguracao")]
+        //public string GetValores()
+        //{
+        //    var valor1 = _configuration["chave1"];
+        //    var valor2 = _configuration ["chave2"];
+        //    var secao1 = _configuration ["secao1:chave2"];
+
+        //    return $"Chave1 = {valor1} \nChave2 = {valor2} \nSeção1 => Chave2 = {secao1}";
+        //}
 
         [HttpGet("produtos")]
         public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
         {
+            _logger.LogInformation("=======================GET api/categorias/produtos =============================");
+
             return _context.Categorias.Include(p => p.Produtos).ToList();
         }
 
         [HttpGet]
+        [ServiceFilter(typeof(ApiLoggingFilter))]   
         public ActionResult<IEnumerable<Categoria>> Get()
         {
             try
             {
+                _logger.LogInformation("=======================GET api/categorias =============================");
                 return _context.Categorias.AsNoTracking().ToList();
             }
             catch (Exception)
@@ -42,10 +61,14 @@ namespace APICatalogo.Controllers
         public ActionResult<Categoria> Get(int id)
         {
             try
-            {
+            {                
                 var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+
+                _logger.LogInformation($"=======================GET api/categorias/id = {id} =============================");
+
                 if (categoria is null)
                 {
+                    _logger.LogInformation($"=======================GET api/categorias/id = {id} NOT FOUND =============================");
                     return NotFound("Categoria não encontrada!");
                 }
                 return categoria;
--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,4 +1,6 @@
 using APICatalogo.Context;
+using APICatalogo.Extensions;
+using APICatalogo.Filters;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 
@@ -16,6 +18,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ApiLoggingFilter>();
 
 var app = builder.Build();
 
@@ -24,6 +27,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.ConfigureExceptionHandler();
 }
 
 app.UseHttpsRedirection();
